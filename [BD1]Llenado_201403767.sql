034c1a3a65d74b9bf822cd9168e7105a7329957f
pablo <pabloramirez.11294@gmail.com>
2020-07-04 13:49:51 -0600

    Entrega final del proyecto. Agregando los últimos cambios.

--- [BD1]Llenado_201403767.sql
+++ [BD1]Llenado_201403767.sql
@@ -2,6 +2,7 @@ use proy2;
 
 DROP TABLE IF EXISTS votacion;
 DROP TABLE IF EXISTS votante;
+DROP TABLE IF EXISTS puesto_partido;
 DROP TABLE IF EXISTS partido;
 DROP TABLE IF EXISTS puesto;
 DROP TABLE IF EXISTS eleccion;
@@ -18,7 +19,9 @@ CREATE TABLE eleccion(
     id_eleccion INTEGER PRIMARY KEY AUTO_INCREMENT,
     nombre VARCHAR(50) NOT NULL,
     pais INTEGER NOT NULL,
-    FOREIGN KEY(pais) REFERENCES zona(id_zona)
+    year INTEGER NOT NULL,
+    FOREIGN KEY(pais) REFERENCES zona(id_zona),
+    CHECK (year>=1900)
 );
 
 CREATE TABLE puesto(
@@ -33,9 +36,7 @@ CREATE TABLE puesto(
 CREATE TABLE partido(
     id_partido INTEGER PRIMARY KEY AUTO_INCREMENT,
     alias VARCHAR(20) NOT NULL,
-    nombre VARCHAR(50) NOT NULL,
-    id_puesto INTEGER NOT NULL,
-    FOREIGN KEY(id_puesto) REFERENCES puesto(id_puesto) 
+    nombre VARCHAR(50) NOT NULL    
 );
 
 CREATE TABLE votante(
@@ -44,6 +45,28 @@ CREATE TABLE votante(
     raza VARCHAR(50) NOT NULL
 );
 
+CREATE TABLE votacion(
+    id_puesto INTEGER,
+    id_partido INTEGER,
+    id_votante INTEGER,
+    analfabetos INTEGER NOT NULL,
+    alfabetos INTEGER NOT NULL,
+    primaria INTEGER NOT NULL,
+    nivel_medio INTEGER NOT NULL,
+    universitarios INTEGER NOT NULL,
+    PRIMARY KEY(id_puesto,id_partido,id_votante),
+    FOREIGN KEY(id_puesto) REFERENCES puesto(id_puesto),
+    FOREIGN KEY(id_partido) REFERENCES partido(id_partido),
+    FOREIGN KEY(id_votante) REFERENCES votante(id_votante), 
+    CHECK (analfabetos>=0),
+    CHECK (alfabetos>=0),
+    CHECK (primaria>=0),
+    CHECK (nivel_medio>=0),
+    CHECK (universitarios>=0)
+);
+
+
+/*
 CREATE TABLE votacion(
     id_partido INTEGER,
     id_votante INTEGER,
@@ -61,7 +84,7 @@ CREATE TABLE votacion(
     CHECK (nivel_medio>=0),
     CHECK (universitarios>=0)
 );
-
+*/
 
 INSERT INTO zona(nombre,padre,tipo)
     SELECT distinct pais,null,'p'
@@ -70,14 +93,14 @@ INSERT INTO zona(nombre,padre,tipo)
 INSERT INTO zona(nombre,padre,tipo)
     SELECT distinct region,id_zona,'r'
     FROM temporal
-    JOIN zona as z on z.nombre=pais;
+    JOIN zona as z on z.nombre=pais and z.tipo='p';
 
 INSERT INTO zona(nombre,padre,tipo)
     SELECT depto,z2.id_zona,'d'
     FROM temporal
     join zona as z on z.nombre=pais and z.tipo='p'
     join zona as z2 on  z2.nombre=region  and z.id_zona=z2.padre and  z2.tipo='r'
-    group by depto;
+    group by depto,pais;
 
 
 INSERT INTO zona(nombre,padre,tipo)
@@ -86,11 +109,40 @@ INSERT INTO zona(nombre,padre,tipo)
     join zona as z on z.nombre=pais and z.tipo='p'
     join zona as z2 on  z2.nombre=region and z.id_zona=z2.padre  and  z2.tipo='r'
     join zona as z3 on z3.nombre=depto and z2.id_zona=z3.padre and z3.tipo='d'
-    group by municipio,z2.id_zona;
+    group by pais,region,depto,municipio;
 
 
-INSERT INTO eleccion(nombre,pais)
-    SELECT distinct nombre_eleccion,z.id_zona
+INSERT INTO eleccion(nombre,pais,year)
+    SELECT distinct nombre_eleccion,z.id_zona, YEAR_ELECCION
     FROM temporal as t
     join zona as z on t.pais=z.nombre and z.tipo='p';
+
+INSERT INTO puesto(id_eleccion,id_zona,tipo)
+    SELECT e.id_eleccion,z4.id_zona,'m'
+    FROM temporal as t
+    join zona as z on z.nombre=pais and z.tipo='p'
+    join zona as z2 on  z2.nombre=region and z.id_zona=z2.padre  and  z2.tipo='r'
+    join zona as z3 on z3.nombre=depto and z2.id_zona=z3.padre and z3.tipo='d'
+    join zona as z4 on z4.nombre=municipio and z3.id_zona=z4.padre and z4.tipo='m'
+    join eleccion as e on z.id_zona=e.pais
+    group by t.pais,region,depto,municipio;
+
+INSERT INTO partido(alias,nombre)
+    SELECT distinct partido,nombre_partido
+    FROM temporal;
+
+INSERT INTO votante(sexo,raza)
+    SELECT distinct sexo,raza
+    FROM temporal;
+
+INSERT INTO votacion
+    SELECT p.id_puesto,par.id_partido,v.id_votante,ANALFABETOS,ALFABETOS,PRIMARIA,NIVEL_MEDIO,UNIVERSITARIOS
+    FROM temporal AS t
+    join zona as z on z.nombre=pais and z.tipo='p'
+    join zona as z2 on  z2.nombre=region and z.id_zona=z2.padre  and  z2.tipo='r'
+    join zona as z3 on z3.nombre=depto and z2.id_zona=z3.padre and z3.tipo='d'
+    join zona as z4 on z4.nombre=municipio and z3.id_zona=z4.padre and z4.tipo='m'
+    join puesto as p on p.id_zona = z4.id_zona
+    join partido as par on par.alias=partido
+    join votante as v on t.sexo=v.sexo and t.raza=v.raza;
 
